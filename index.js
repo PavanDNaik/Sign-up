1499e575669cae8b026ef5e30e88ea2658c056fc
PavanDNaik <pawannaik396@gmail.com>
2023-10-14 17:53:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let password = document.getElementById("pass");
+let confirms = document.getElementById("conf");
+
+password.oninput = verification;
+confirms.oninput = verification;
+function verification(){
+
+    if(password.value == confirms.value || 
+    (!password.classList.contains("error") ||
+    !confirms.classList.contains("error")))
+    {
+        password.classList.toggle("error");
+        confirms.classList.toggle("error");
+        password.classList.toggle("proper");
+        confirms.classList.toggle("proper");
+    }
+}
